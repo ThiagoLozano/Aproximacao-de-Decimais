ca1c2f1ee6b6d81b865bbada6dc7a239345e5a0d
ThiagoLozano <51161587+ThiagoLozano@users.noreply.github.com>
2020-09-30 20:54:45 -0300

    Programa Python

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 class Aproximacao:
     def __init__(self):
-        self.soma = 0
-        self.bits = 0
+        self.soma_a_menor = 0
+        self.qnt_bits = 0
         while True:
             # Valida o dado de entrada.
             try:
@@ -20,19 +20,20 @@ class Aproximacao:
         while True:
             # Valida a escolha do usuário.
             try:
-                self.bits = int(input("Escolha um quantidade de bits(5 até 12): "))
-                if self.bits < 5 or self.bits > 12:
+                self.qnt_bits = int(input("Escolha um quantidade de bits(5 até 12): "))
+                if self.qnt_bits < 5 or self.qnt_bits > 12:
                     print("Erro: Insira apenas valores inteiros entre 5 e 12.\n")
                 else:
-                    self.A_menor(self.decimal, self.bits)
+                    self.A_menor(self.decimal, self.qnt_bits)
+                    self.A_maior()
                     break
             except ValueError:
                 print("Erro: Tipo de dado inválido\n")
 
-    # Retorna a aproximação a menor.
+    # Retorna a aproximações A Menor.
     def A_menor(self, valor_decimal, bit):
         valor_binario = []
-        # Faz a multiplicação até gerar os valores binários.
+        # Faz as multiplicações sucessivas até gerar os valores binários.
         for c in range(bit):
             valor_decimal * 2
             if int(valor_decimal) == 1:
@@ -45,44 +46,12 @@ class Aproximacao:
         v = ''
         for c in valor_binario:
             v += str(c)
-        # Mostra o valor aproximado em binário.
+        # Retorna o valor Binário para a aproximação A Menor.
         print('({})2'.format(v), end='')
-        self.Converter_para_Decimal(valor_binario)
-        self.A_maior()
+        self.Decimal_A_Menor(valor_binario)
 
-    # Retorna a aproximação a maior.
-    def A_maior(self):
-        """
-        Para mostrar o valor Binário de "A maior", preciso somar o Binário
-        de "A menor" conforme a quantidade de bits que tiver,
-        Ex: 5 bits
-        Binário de "A menor" + 00001
-        Ex: 7 bits
-        Binário de "A menor" + 0000001
-        Obs: Só falta apenas esse pedaço.
-        """
-        print("A Maior: ", end='')
-        print('()2'.format(), end='')
-        binario = []
-        decimal = []
-        # Cria o Binário que corresponde ao número de bits para a soma em decimal.
-        for c in range(self.bits):
-            binario.append(0)
-        ultimo = len(binario) - 1
-        binario[ultimo] = 1
-        # Converte o Binário em Decimal.
-        for i, v in enumerate(binario):
-            if v == 1:
-                decimal.append(2 ** (-i - 1))
-        a_menor = self.soma
-        decimal.append(a_menor)
-        # Soma com o valor de A menor já descoberto.
-        soma = sum(decimal)
-        print(' ({})10'.format(soma), end=' ')
-        self.Erro(self.decimal, soma)
-
-    # Converte os valor binário para de decimal da aproximação a menor.
-    def Converter_para_Decimal(self, binario):
+    # Retorna o valor Decimal para a aproximação A Menor.
+    def Decimal_A_Menor(self, binario):
         decimal = []
         # Converte o Binário em um Decimal com vírgula.
         for i, v in enumerate(binario):
@@ -90,13 +59,73 @@ class Aproximacao:
                 # Pega os valores exponenciais correspondentes de cada posição
                 decimal.append(2 ** -(i + 1))
         # Faz a soma total dos expoentes.
-        self.soma = sum(decimal)
+        self.soma_a_menor = sum(decimal)
         # Retorna a soma de cada posição.
-        print(' ({})10'.format(self.soma), end=' ')
-        self.Erro(self.decimal, self.soma)
+        print(' ({})10'.format(self.soma_a_menor), end=' ')
+        self.Erro(self.decimal, self.soma_a_menor)
 
-    @staticmethod
+    # Retorna as aproximações A Maior.
+    def A_maior(self):
+        """ Etapa 1: Gera Binário """
+        binario = []
+        decimal = []
+        # Cria o Binário que corresponde ao número de bits para a soma em decimal.
+        for c in range(self.qnt_bits):
+            binario.append(0)
+        ultimo = len(binario) - 1
+        binario[ultimo] = 1
+
+        """ Etapa 2: Converte Binário em Decimal """
+        # Converte o Binário em Decimal.
+        for i, v in enumerate(binario):
+            if v == 1:
+                decimal.append(2 ** (-i - 1))
+        a_menor = self.soma_a_menor
+        decimal.append(a_menor)
+        
+        """ Etapa 3: Soma o Decimal 'A Menor' com o Decimal do Binário """
+        # Soma com o valor de A menor já descoberto.
+        soma = sum(decimal)
+
+        """ Etapa 4: Converte a Soma do Decimal em Binário """
+        valor_binario = []
+        # Faz as multiplicações sucessivas até gerar os valores binários.
+        for c in range(self.qnt_bits):
+            soma * 2
+            if int(soma) == 1:
+                soma = soma - 1
+            soma = soma * 2
+            valor_binario.append(int(soma))
+        print("A Maior: ", end='')
+        v = ''
+        for c in valor_binario:
+            v += str(c)
+        # Retorna o valor Binário para a aproximação A Maior.
+        print('({})2'.format(v), end='')
+        self.Decimal_A_Maior()
+
+    # Retorna o valor Decimal para a aproximação A Maior.
+    def Decimal_A_Maior(self):
+        binario = []
+        decimal = []
+        # Cria o Binário que corresponde ao número de bits para a soma em decimal.
+        for c in range(self.qnt_bits):
+            binario.append(0)
+        ultimo = len(binario) - 1
+        binario[ultimo] = 1
+        # Converte o Binário em Decimal.
+        for i, v in enumerate(binario):
+            if v == 1:
+                decimal.append(2 ** (-i - 1))
+        a_menor = self.soma_a_menor
+        decimal.append(a_menor)
+        # Soma com o valor de A menor já descoberto.
+        soma = sum(decimal)
+        print(' ({})10'.format(soma), end=' ')
+        self.Erro(self.decimal, soma)
+
     # Calcula a porcentagem do erro de aproximação.
+    @staticmethod
     def Erro(nd, no):
         erro = (nd - no) / nd
         if erro < 0:
